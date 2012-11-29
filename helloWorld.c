a7007a3a5eea9d07abac46130cd079ce5ef146a9
Raymond John Hill <raymondjohnhill@gmail.com>
2012-11-29 18:54:52 -0500

    Edited: Hello World

--- helloWorld.c
+++ helloWorld.c
@@ -1,12 +1,21 @@
+//This includes the Standard Input and Output Library
 #include <stdio.h>
 
+//This is your main function.
 int main(int argc, char *argv[])
 {
+
+  //This simply outputs this line to a stream.
   puts("Hello World!");
 
-  char* printMe = "Hello, hello, hello world!";
-
-  puts(printMe);
-
+  //Returns zero to end the program.
   return 0;
 }
+
+//Instructions for running:
+//(assuming you have a C compiler installed)
+//0.save this file as helloWorld.c
+//1.open terminal or cmd
+//2.cd to the directory that contains this file
+//3.type 'make helloWorld'
+//4.now type './helloWorld'
